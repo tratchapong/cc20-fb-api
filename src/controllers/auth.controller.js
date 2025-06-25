4234c7571470985d22d01e391bd0df628b85e760
tratchapong <tratchapong@gmail.com>
2025-06-25 10:15:06 +0700

    register api : done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,11 +1,44 @@
+import bcrypt from 'bcryptjs'
 import prisma from '../config/prisma.config.js'
+import checkIdentity from '../utils/check-identity.util.js'
 import createError from '../utils/create-error.util.js'
 
 export async function register(req, res, next) {
-	res.json({
-		msg: 'Register controller',
-		body: req.body
-	})
+	try {
+		const { identity, firstName, lastName, password, confirmPassword } = req.body
+		//validation
+		if (!(identity.trim() && firstName.trim() && lastName.trim() && password.trim() && confirmPassword.trim())) {
+			createError(400, 'Please fill all data')
+		}
+		if (password !== confirmPassword) {
+			createError(400, 'check confirm password')
+		}
+		// identity เป็น email หรือ mobile phone number : checkIdentity(identity) => String : 'email' | 'mobile'
+		const identityKey = checkIdentity(identity)
+
+		// หา user
+		const foundUser = await prisma.user.findUnique({
+			where: { [identityKey]: identity }
+		})
+		if (foundUser) {
+			createError(409, `Already have this user: ${identity}`)
+		}
+
+		const newUser = {
+			[identityKey]: identity,
+			password: await bcrypt.hash(password, 10),
+			firstName: firstName,
+			lastName: lastName
+		}
+
+		const result = await prisma.user.create({ data: newUser })
+		res.json({
+			msg: 'Register controller',
+			result: result
+		})
+	} catch (err) {
+		next(err)
+	}
 }
 
 export const login = (req, res, next) => {
---
+++ src/utils/check-identity.util.js
@@ -0,0 +1,20 @@
+import createError from "./create-error.util.js"
+
+export default function (identity) {
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+	const mobileRegex = /^[0-9]{10,15}$/
+
+	let identityKey = ''
+	if(emailRegex.test(identity)) {
+		identityKey = 'email'
+	}
+	if(mobileRegex.test(identity)) {
+		identityKey = 'mobile'
+	}
+	if(!identityKey) {
+		createError(400, 'identity only accept Email or Mobile phone')
+	}
+	return identityKey
+
+	// return emailRegex.test(identity) ? 'email' : mobileRegex.test(identity) ? 'mobile' : null
+}
