625c167d308feb67662a4da5f0f32c47f9f1fb11
tratchapong <tratchapong@gmail.com>
2025-06-30 16:23:10 +0700

    cloudinary start..

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,3 +1,5 @@
+import path from 'path'
+import cloudinary from "../config/cloudinary.config.js"
 
 export const getAllPosts = async (req,res,next) => {
 
@@ -5,11 +7,21 @@ export const getAllPosts = async (req,res,next) => {
 }
 
 export const createPost = async (req,res,next) => {
-	console.log(req.body.message)
+	const {message} = req.body
 	console.log(req.file)
+	let haveFile = !!req.file
+	let uploadResult = null
+	if(haveFile) {
+		uploadResult = await cloudinary.uploader.upload(req.file.path),{
+			overwrite : true,
+			public_id : path.parse(req.file.path).name
+		}
+	}
+	console.log(uploadResult)
 	res.json({
 		message: 'Create post',
-		file : req.file
+		file : req.file,
+		uploadResult
 	})
 }
 
